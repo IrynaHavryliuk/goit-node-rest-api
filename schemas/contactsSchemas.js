656c5bdc21547e301bd82c8f68d086c43648d2fe
Iryna Havryliuk <gis.80@ukr.net>
2024-04-28 18:12:01 -0400

    add

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,17 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().email().required(),
+  address: Joi.string().required(),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  firstName: Joi.string(),
+  lastName: Joi.string(),
+  phone: Joi.string(),
+  email: Joi.string().email(),
+  address: Joi.string(),
+}).min(1); 
